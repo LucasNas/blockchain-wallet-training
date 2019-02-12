a4be998e20c85803d22f9d632049847414fdc1ba
Lucas Guilherme Nascimento <lucas.borges@wipro.com>
2019-02-12 11:08:04 -0200

    further code development

--- projects/blockchain/src/lib/models/blockchain.ts
+++ projects/blockchain/src/lib/models/blockchain.ts
@@ -1,9 +1,60 @@
 import { Block } from './block';
 import { Transaction } from './transaction';
+import { v4 as uuid } from 'uuid';
+import { BlockData } from './block-data';
+import { sha256 } from  'hash.js';
+
 
 export class Blockchain {
     public chain: Block[];
     public pendingTransactions: Transaction[] = [];
     public nodeUrl: string;
     public networkNodes: string[] = [];
+    private difficulty: number;
+
+    constructor(GENESIS_BLOCK: Block, difficulty = 2){
+        this.chain = [GENESIS_BLOCK];
+        this.nodeUrl = uuid();
+        this.difficulty=difficulty;
+    }
+
+    newBlock(nonce:string | number, previousHash: string, hash: string): Block{
+        const newBlock= new Block(
+            this.chain.length +1,
+            Date.now(),
+            this.pendingTransactions,
+            nonce.toString(),
+            hash,
+            previousHash
+        );
+
+        this.pendingTransactions= [];
+        this.chain.push(newBlock);
+        return newBlock
+    }
+
+    getLatestBlock():Block{
+        return this.chain[this.chain.length-1];
+    }
+
+    hashBlock(previousHash: string, blockData: BlockData, nonce: string | number): string{
+        const data = previousHash
+        + JSON.stringify(blockData)
+        + nonce.toString();
+
+        const hash= sha256().update(data).digest('hex');
+        return hash;
+    }
+
+    proofOfWorkNonce(previousHash: string, currentBlockData:BlockData): string{
+        let nonce= 0;
+        let hash= this.hashBlock(previousHash,currentBlockData,nonce);
+
+        while(hash.substr(0,this.difficulty) !== this.chain[0].hash.substr(0,this.difficulty)){
+            nonce++;
+            hash = this.hashBlock(previousHash,currentBlockData,nonce);
+        }
+
+        return nonce.toString();
+    }
 }
